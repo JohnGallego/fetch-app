b5a04a35dcc40f57ca501957ae4029ecf2fa66fa
John Gallego <johngallegodev@gmail.com>
2025-02-06 15:13:53 -0500

    Http request setup for testing

---
+++ src/constants/routes.ts
@@ -0,0 +1,16 @@
+export const ROUTES = {
+  API: "https://frontend-take-home-service.fetch.com",
+  AUTH: {
+    LOGIN: "/auth/login",
+    LOGOUT: "/auth/logout",
+  },
+  DOGS: {
+    BREEDS: "/dogs/breeds",
+    SEARCH: "/dogs/search",
+    GET: "/dogs",
+    MATCH: "/dogs/match",
+  },
+  LOCATION: {
+    GET: "/locations"
+  }
+};
